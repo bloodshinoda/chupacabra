388201a7f2e1fd7b3eceb879d6e49dc30625de5d
iBlood Heuer <bloodshinoda@gmail.com>
2026-09-17 13:50:47 -0300

    Monta o runtime do engine na árvore React

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -11,6 +11,7 @@ import { useEffect, type ReactNode } from "react";
 
 import appCss from "../styles.css?url";
 import { reportLovableError } from "../lib/lovable-error-reporting";
+import { EngineRuntime } from "../components/engine-runtime";
 
 function NotFoundComponent() {
   return (
@@ -119,6 +120,7 @@ function RootComponent() {
 
   return (
     <QueryClientProvider client={queryClient}>
+      <EngineRuntime />
       {/* Required: nested routes render here. Removing <Outlet /> breaks all child routes. */}
       <Outlet />
     </QueryClientProvider>
