2d91162a5ff9c871ae7a1bbb2dc3abef50a4ddd5
iBlood Heuer <bloodshinoda@gmail.com>
2026-09-21 10:12:29 -0300

    Adiciona provider geográfico internacional

--- engine/geography/__init__.py
+++ engine/geography/__init__.py
@@ -1,6 +1,6 @@
 """Geographic catalog providers for target planning."""
-
 from engine.geography.ibge import IbgeBrazilCatalog
 from engine.geography.models import TargetLocation
+from engine.geography.world import WorldCityCatalog
 
-__all__ = ["IbgeBrazilCatalog", "TargetLocation"]
+__all__ = ["IbgeBrazilCatalog", "TargetLocation", "WorldCityCatalog"]
